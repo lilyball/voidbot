6f7ed1b4ff97c5908118bdc50403424432bd6da1
Kevin Ballard <kevin@sb.org>
2013-04-08 21:51:37 -0700

    Add runtime control for derpcoin

--- plugin/derpcoin/derpcoin.go
+++ plugin/derpcoin/derpcoin.go
@@ -13,15 +13,39 @@ func init() {
 	plugin.RegisterSetup(setup)
 }
 
+var enabled = true
+
 func setup(conn *irc.Conn, er event.EventRegistry) error {
 	conn.AddHandler("PRIVMSG", func(conn *irc.Conn, line *irc.Line) {
 		text := line.Args[len(line.Args)-1]
 		dst := line.Args[0]
 
 		if strings.HasPrefix(dst, "#") {
-			text = ReplaceAllFold(text, "bitcoin", "derpcoin")
-			if text != "" {
-				plugin.Conn(conn).Privmsg(dst, text)
+			comps := strings.SplitN(text, " ", 2)
+			if comps[0] == "!derpcoin" {
+				if len(comps) > 1 {
+					flag := strings.ToLower(comps[1])
+					if flag == "on" {
+						enabled = true
+						plugin.Conn(conn).Privmsg(dst, "derpcoin enabled")
+					} else if flag == "off" {
+						enabled = false
+						plugin.Conn(conn).Privmsg(dst, "derpcoin diasbled")
+					}
+				} else {
+					msg := "derpcoin is: "
+					if enabled {
+						msg += "ON"
+					} else {
+						msg += "OFF"
+					}
+					plugin.Conn(conn).Privmsg(dst, msg)
+				}
+			} else if enabled {
+				text = ReplaceAllFold(text, "bitcoin", "derpcoin")
+				if text != "" {
+					plugin.Conn(conn).Privmsg(dst, text)
+				}
 			}
 		}
 	})
