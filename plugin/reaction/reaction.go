5da07fbc72f16fee6761425fa758b8f019969b7b
Kevin Ballard <kevin@sb.org>
2013-04-19 21:17:23 -0700

    Me1000 is always drunk

--- plugin/reaction/reaction.go
+++ plugin/reaction/reaction.go
@@ -3,8 +3,10 @@ package reaction
 import (
 	"../"
 	"../../utils"
+	"fmt"
 	"github.com/kballard/gocallback/callback"
 	"github.com/kballard/goirc/irc"
+	"math/rand"
 	"strings"
 )
 
@@ -22,10 +24,22 @@ func setup(hreg irc.HandlerRegistry, reg *callback.Registry) error {
 			// what, the server sent us a privmsg?
 			return
 		}
+		// allow voidbot to be addressed directly, and modify the response
+		prefix := ""
+		if strings.HasPrefix(text, fmt.Sprintf("%s: ", conn.Me().Nick)) {
+			text = text[len(conn.Me().Nick)+2:]
+			prefix = line.Src.Nick + ": "
+		}
 		// implement super awesome reaction logic here
 		if strings.ToLower(text) == "herp" {
-			plugin.Conn(conn).Privmsg(reply, utils.MatchCase("derp", text))
+			plugin.Conn(conn).Privmsg(reply, prefix+utils.MatchCase("derp", text))
+		} else if strings.ToLower(text) == "is me1000 drunk?" || (line.Src.Nick == "Me1000" && strings.ToLower(text) == "am i drunk?") {
+			plugin.Conn(conn).Privmsg(reply, prefix+randResponse([]string{"yes", "always"}))
 		}
 	})
 	return nil
+}
+
+func randResponse(opts []string) string {
+	return opts[rand.Intn(len(opts))]
 }
