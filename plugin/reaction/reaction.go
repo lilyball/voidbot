5d1e77b4acfddb9c253a217666734d587424eecc
Kevin Ballard <kevin@sb.org>
2013-05-30 17:04:16 -0700

    reaction: add botsnack

--- plugin/reaction/reaction.go
+++ plugin/reaction/reaction.go
@@ -26,15 +26,19 @@ func setup(reg *callback.Registry) error {
 		}
 		// allow voidbot to be addressed directly, and modify the response
 		prefix := ""
+		isDirected := false
 		if strings.HasPrefix(text, fmt.Sprintf("%s: ", conn.Me().Nick)) {
 			text = text[len(conn.Me().Nick)+2:]
 			prefix = line.Src.Nick + ": "
+			isDirected = true
 		}
 		// implement super awesome reaction logic here
 		if strings.ToLower(text) == "herp" {
 			plugin.Conn(conn).Privmsg(reply, prefix+utils.MatchCase("derp", text))
 		} else if strings.ToLower(text) == "is me1000 drunk?" || (line.Src.Nick == "Me1000" && strings.ToLower(text) == "am i drunk?") {
 			plugin.Conn(conn).Privmsg(reply, prefix+randResponse([]string{"yes", "always"}))
+		} else if isDirected && strings.ToLower(text) == "botsnack" {
+			plugin.Conn(conn).Privmsg(reply, prefix+randResponse([]string{"yum", "nom nom", "om nom nom"}))
 		}
 	})
 	return nil
